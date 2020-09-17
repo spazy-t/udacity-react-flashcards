6543d1d9cf09ca45487c2dc77887f0b9b7178b36
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-09-17 20:02:09 +0100

    chore: add navigation depnedencies

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from "react"
+import { View, Text } from "react-native"
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
     <View>
       <Text>Flashcards</Text>
+      <NavigationContainer>
+        
+      </NavigationContainer>
     </View>
   )
 }
